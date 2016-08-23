7e5be9b4cf6c1417de06f422152c20d0297a164d
Zuidjed <sjarreldevis@gmail.com>
2016-08-23 20:15:46 +0200

    Add files via upload

---
+++ files.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'assets/files.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+          <title>FlyEurope - Files</title>
+    </head>
+        <body>
+        	<div class="f-text">
+        		<div id="f-text-1">
+        			Files Page
+        			<div id="f-text-2">
+        				On this page you can find files about the OFP of the groupflight<p> we are doing, settings from P3D,OBS etc.
+        			</div>
+        		</div>
+        	</div>
+
+        		<!-- Downloads -->
+        	<div class="f-text1">
+        		<div id="f-text-3">
+        			FlightPlans
+        			<div id="f-text-4">
+       					<a href="files/LGW-ZRH.pdf">Groupflight: &nbsp London Gatwick LGW -> Zurich ZRH (23-8-2016)<p></a>
+                            <div id="f-text-5">
+                                <a href="files/SMI-RHO.pdf">Groupflight: &nbsp Samos SMI -> Rhodes RHO (24-08-2016)<p></a>
+                            </div>
+                        </div>
+        			</div>
+         		</div>
+         </body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+include 'assets/files.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+          <title>FlyEurope - Home</title>
+    </head>
+        <body>
+
+                <div id="index-text-1">
+                    Hello and welcome to the website of FlyEurope!
+                </div>
+
+                <div id="index-text-2">
+                    On this website you can find a lot about us and about FlyEurope!
+                </div>
+
+                <div class="index-text-3-1">
+                    What is FlyEurope?
+                </div>  
+
+                <div id="index-text-3-2">
+                    FlyEurope is a YouTube channel made by Joel and Michel. They both fly alot on<p>
+                    Prepar3D and they both stream some of their flights on Twitch.tv<p>
+                </div>
+
+	            <div class="index-text-3-3">
+	                Welcome on our website and have a pleasure stay!
+	            </div>
+
+	           <div class="i-text">
+	           		<div id="i-text1">
+	           			<div id="i-text2">
+	           				<div id="i-text3-1">
+	           					<div id="i-text3-2">
+	           						<div id="i-text3-3">\
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </body>
+</html>
