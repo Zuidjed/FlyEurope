2d2e6777d00040f701ce553f2f91054c22bd4bf6
Zuidjed <sjarreldevis@gmail.com>
2016-08-24 21:50:22 +0200

    Add files via upload

---
+++ twitch.php
@@ -0,0 +1,16 @@
+<?php
+
+include 'assets/files.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+          <title>FlyEurope - Twitch</title>
+    </head>
+        <body>
+	        <div id="twitch-player-1">
+				<iframe src="https://player.twitch.tv/?channel=streamwithmich" frameborder="0" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/streamwithmich?tt_medium=live_embed&tt_content=text_link" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px;text-decoration:underline;"></a>
+	        </div>
+         </body>
+</html>
