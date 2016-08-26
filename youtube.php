285c9b8f701b98fb73a0379a5075e264fb3072a2
Zuidjed <sjarreldevis@gmail.com>
2016-08-26 15:53:57 +0200

    Add files via upload

---
+++ youtube.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'assets/files.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+          <title>FlyEurope - Twitch</title>
+    </head>
+        <body>
+
+        <div id="Parent">
+
+	        <div class="child1">
+	        	Our Latest Video Uploaded to YouTube
+	        </div>
+
+	        <div class="child2">
+	        Recommend Video
+	        </div>
+
+        </div>	
+	        <div class="yt-player-1">
+	        	<iframe width="560" height="315" src="https://www.youtube.com/embed/Zq056Pen6_A" frameborder="0" allowfullscreen></iframe>
+	        </div>
+
+	        <div class="yt-player-2">
+	        	<iframe width="560" height="315" src="https://www.youtube.com/embed/8eg8wmW0KvY" frameborder="0" allowfullscreen></iframe>
+	        </div>
+
+         </body>
+</html>
